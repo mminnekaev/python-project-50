ac933acb693466eb5a89dcaa39c33c9e3579ff06
Marat Minnekaev <marat@Marats-MacBook-Pro.local>
2023-09-17 18:45:40 +0200

    Test fixtures modified

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -3,7 +3,8 @@ from gendiff import generate_diff
 
 
 def test_generate_diff():
-    correct_answer = "{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}"
+    with open('fixtures/correct_answer1') as f:
+        correct_answer = f.read()
 
     assert generate_diff(
         'tests/fixtures/file1.json',
