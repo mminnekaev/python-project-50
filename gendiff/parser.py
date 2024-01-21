4972d24f48c5f2fb09d6cc216c09619cd8818a80
Marat Minnekaev <marat@Marats-MacBook-Pro.local>
2024-01-21 21:09:20 +0100

    Updates according to comments on Jan 20th

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -4,15 +4,14 @@ from os.path import splitext
 
 
 def parse(data, format):
-    if format in {'.yml', '.yaml'}:
+    if format in {'yml', 'yaml'}:
         return yaml.load(data, Loader=yaml.FullLoader)
-    elif format == '.json':
+    elif format == 'json':
         return json.loads(data)
     raise ValueError("This data format is not supported")
 
 
 def get_data(path):
-    format = splitext(path)[1]
+    _, format = splitext(path)
     with open(path) as f:
-        data = f.read()
-    return parse(data, format)
+        return parse(f.read(), format[1:])
